fa28c09feec1116839b9ff25dd9635c9407b3bcb
Steven Roberts <sr320@uw.edu>
2022-02-17 15:59:16 -0800

    rsync

---
+++ rsync.sh
@@ -0,0 +1,5 @@
+
+
+/volume2/web/seashell/bu-mox$ 
+rsync -avz --exclude '*_to_*' --exclude 'CHG_*.txt' --exclude 'CHH_*.txt' --exclude 'CpG_*txt' \
+--progress sr320@mox.hyak.uw.edu:/gscratch/scrubbed/sr320/ scrubbed/
