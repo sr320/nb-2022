f6f1f769c594c35a7cfef4773ac1d2d54c9eeb33
Steven Roberts <sr320@uw.edu>
2022-02-21 12:12:25 -0800

    vcf

--- rsync.sh
+++ rsync.sh
@@ -3,3 +3,12 @@
 /volume2/web/seashell/bu-mox$ 
 rsync -avz --exclude '*_to_*' --exclude 'CHG_*.txt' --exclude 'CHH_*.txt' --exclude 'CpG_*txt' \
 --progress sr320@mox.hyak.uw.edu:/gscratch/scrubbed/sr320/ scrubbed/
+
+
+
+
+
+
+/home/shared/8TB_HDD_01/sr320/github$ 
+rsync -avz . \
+sr320@gannet.fish.washington.edu:/volume2/web/seashell/bu-github/
