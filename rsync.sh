66dc34dc2d37cb9d2b7d3f3aa0ed002c6f3961d0
Steven Roberts <sr320@uw.edu>
2022-05-02 12:00:19 -0700

    Update rsync.sh

--- rsync.sh
+++ rsync.sh
@@ -6,7 +6,8 @@ rsync -avz --exclude '*_to_*' --exclude 'CHG_*.txt' --exclude 'CHH_*.txt' --excl
 
 
 
-
+/volume2/web/seashell/bu-mox$ 
+rsync -avz --progress sr320@mox.hyak.uw.edu:/gscratch/srlab/sr320/ .
 
 
 /home/shared/8TB_HDD_01/sr320/github$ 
